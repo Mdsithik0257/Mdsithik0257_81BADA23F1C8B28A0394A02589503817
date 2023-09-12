70b06ff049c49169532d19edc4a7a28f3ad41b5c
Mdsithik0257 <144802202+Mdsithik0257@users.noreply.github.com>
2023-09-12 19:53:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-#1 implement a recursive function to calculate the factorial of a given number 
-i=1
-fact=1
-n=int(input("Enter the number:"))
-while(i<=n):
-     fact=fact*i
-     i=i+1
-print(fact)
+year=int(input("Enter the year:"))
+if(year%4==0):
+  print(year,"is leap year")
+else:
+  print(year,"non leap year") 
